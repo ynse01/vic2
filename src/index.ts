8c9df5e26584296570905511154a5ceb886c4dae
Ynse Hoornenborg <github@ynse.nl>
2021-02-11 21:10:32 +0100

    Lod texture externally

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { ColorPalette } from "./color-palette";
 import { IBuffers } from "./i-buffers";
 import { IProgramInfo } from "./i-program-info";
 import { Renderer } from "./renderer";
+import { TextureLoader } from "./texture-loader";
 
 // Vertex shader
 const vsSource = `
@@ -117,39 +118,6 @@ function initBuffers(gl: WebGLRenderingContext): IBuffers| null {
     return null;
 }
 
-function loadTexture(gl: WebGLRenderingContext, url: string): WebGLTexture | null {
-    const texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    // Because images have to be downloaded over the internet
-    // they might take a moment until they are ready.
-    // Until then put a single pixel in the texture so we can
-    // use it immediately. When the image has finished downloading
-    // we'll update the texture with the contents of the image.
-    const level = 0;
-    const internalFormat = gl.RGBA;
-    const width = 1;
-    const height = 1;
-    const border = 0;
-    const srcFormat = gl.RGBA;
-    const srcType = gl.UNSIGNED_BYTE;
-    const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
-    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, 
-        width, height, border, srcFormat, srcType, pixel);
-
-    const image = new Image();
-    image.onload = function() {
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, srcFormat, srcType, image);
-        // Assume texture is not power of 2
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    };
-    image.src = url;
-
-    return texture;
-}
-
 function main() {
     const canvas = <HTMLCanvasElement>document.querySelector("#glCanvas");
     if (canvas != null) {
@@ -172,14 +140,14 @@ function main() {
                 }
             };
             // Load the texture
-            const texture = loadTexture(gl, 'cubetexture.png');
-            if (texture != null) {
+            const loader = new TextureLoader(gl, 'cubetexture.png');
+            if (loader.texture != null) {
                 // Here's where we call the routine that builds all the
                 // objects we'll be drawing.
                 const buffers = initBuffers(gl);
                 if (buffers != null) {
                     // Draw the scene
-                    const renderer = new Renderer(gl, programInfo, buffers, texture);
+                    const renderer = new Renderer(gl, programInfo, buffers, loader.texture);
                     renderer.renderLoop();
                 }
             }
