8c9347af9a778ef98937cfc15ff447ee9ed5e34c
Ynse Hoornenborg <github@ynse.nl>
2021-02-19 15:34:38 +0100

    Fix character display

--- src/data-loader.ts
+++ src/data-loader.ts
@@ -46,6 +46,7 @@ export class DataLoader {
                     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
                     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
                     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
                     this._createTable(byteArray);
                 };
                 request.send(null);
