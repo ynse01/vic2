1c32a9e4eb12f5774fdbeff3cc35e3b830a70b4c
Ynse Hoornenborg <github@ynse.nl>
2021-02-23 20:20:16 +0100

    Construct from array also

--- src/data-texture.ts
+++ src/data-texture.ts
@@ -5,14 +5,21 @@
  */
 export class DataTexture {
     private _gl: WebGLRenderingContext;
-    private _url: string;
+    private _url: string | null;
+    private _data: number[] | null;
     private _attributeName: string;
     private _texture: WebGLTexture | null;
     private _buffer: WebGLBuffer | null;
 
-    constructor(gl: WebGLRenderingContext, url: string, attributeName: string) {
+    constructor(gl: WebGLRenderingContext, data: string | number[], attributeName: string) {
         this._gl = gl;
-        this._url = url;
+        if (typeof data === 'string') {
+            this._url = <string>data;
+            this._data = null;
+        } else {
+            this._url = null;
+            this._data = <number[]>data;
+        }
         this._attributeName = attributeName;
         this._texture = null;
         this._buffer = null;
@@ -34,33 +41,23 @@ export class DataTexture {
                 // Until then put a single pixel in the texture so we can
                 // use it immediately. When the image has finished downloading
                 // we'll update the texture with the contents of the image.
-                const level = 0;
-                const internalFormat = gl.LUMINANCE;
-                const srcFormat = gl.LUMINANCE;
-                const srcType = gl.UNSIGNED_BYTE;
-                const pixel = new Uint8Array([0]);
-                gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, 
-                    1, 1, 0, srcFormat, srcType, pixel);
+                const width = 8;
+                const height = 256;
+                if (this._url != null) {
+                    this.texImage(new Uint8Array([0]), 1, 1);
+                    const request = new XMLHttpRequest();
+                    request.open("GET", this._url, true);
+                    request.responseType = "arraybuffer";
 
-                const request = new XMLHttpRequest();
-                request.open("GET", this._url, true);
-                request.responseType = "arraybuffer";
-
-                request.onload = () => {
-                    const arrayBuffer = request.response;
-                    const byteArray = new Uint8Array(arrayBuffer);
-                    const width = 8;
-                    const height = 256;
-                    const border = 0;
-                    gl.bindTexture(gl.TEXTURE_2D, texture);
-                    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border, srcFormat, srcType, byteArray);
-                    // Assume texture is not power of 2
-                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-                };
-                request.send(null);
+                    request.onload = () => {
+                        const arrayBuffer = request.response;
+                        const byteArray = new Uint8Array(arrayBuffer);
+                        this.texImage(byteArray, width, height);
+                    };
+                    request.send(null);
+                } else if (this._data != null) {
+                    this.texImage(new Uint8Array(this._data), width, height);
+                }
             }
         }
         return this._texture;
@@ -82,7 +79,7 @@ export class DataTexture {
 
     public enable(shaderProgram: WebGLProgram): void {
         const gl = this._gl;
-        const attribLocation = gl.getAttribLocation(shaderProgram, 'aCharacterRomCoord');
+        const attribLocation = gl.getAttribLocation(shaderProgram, this._attributeName);
         // Tell WebGL how to pull out the texture coordinates from
         // the texture coordinate buffer into the textureCoord attribute.
         const numComponents = 2;
@@ -105,5 +102,21 @@ export class DataTexture {
         const gl = this._gl;
         gl.activeTexture(index);
         gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    }
+
+    private texImage(byteArray: Uint8Array, width: number, height: number) {
+        const gl = this._gl;
+        const level = 0;
+        const internalFormat = gl.LUMINANCE;
+        const srcFormat = gl.LUMINANCE;
+        const srcType = gl.UNSIGNED_BYTE;
+        const border = 0;
+        gl.bindTexture(gl.TEXTURE_2D, this._texture);
+        gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border, srcFormat, srcType, byteArray);
+        // Assume texture is not power of 2
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     }
 }
